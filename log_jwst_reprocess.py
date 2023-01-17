249107cbaf4e67bfd093fe7b6c14ca7a1e4dac3a
Thomas WIlliams <williams@mpia.de>
2023-01-17 08:39:15 +0000

    Update log_jwst_reprocess.py to use local.toml

--- log_jwst_reprocess.py
+++ log_jwst_reprocess.py
@@ -1,21 +1,42 @@
 import os
 import socket
 from datetime import datetime
+import sys
 
-host = socket.gethostname()
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
-if 'node' in host:
-    working_dir = '/data/beegfs/astro-storage/groups/schinnerer/williams/jwst_phangs_reprocessed'
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+if len(sys.argv) == 1:
+    config_file = script_dir + '/config/config.toml'
+    local_file = script_dir + '/config/local.toml'
+
+elif len(sys.argv) == 2:
+    config_file = sys.argv[1]
+    local_file = script_dir + '/config/local.toml'
+
+elif len(sys.argv) == 3:
+    config_file = sys.argv[1]
+    local_file = sys.argv[2]
+
 else:
-    working_dir = '/Users/williams/Documents/phangs/jwst_reprocessed'
+    raise Warning('Cannot parse %d arguments!' % len(sys.argv))
+
+with open(local_file, 'rb') as f:
+    local = tomllib.load(f)
 
 date_str = datetime.today().strftime('%Y%m%d_%H:%M:%S')
+
+working_dir = local['local']['working_dir']
 
 log_file = os.path.join(working_dir,
                         'jwst_reprocess_log_%s.log' % date_str)
 
 script_name = 'run_jwst_reprocessing.py'
 
-os.system('python3 %s |& tee %s' % (script_name, log_file))
+os.system('python3 %s %s %s |& tee %s' % (script_name, config_file, local_file, log_file))
 
 print('Complete!')
